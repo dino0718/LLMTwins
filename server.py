f7bfe5bc2c758d15ab02efd152b515b8019254e0
黃俊毓 <huangjunyu@huangjunyudeMacBook-Pro.local>
2024-12-30 19:25:08 +0800

    refact: agent tools

--- server.py
+++ server.py
@@ -4,8 +4,6 @@ from fastapi.middleware.cors import CORSMiddleware
 from phi.agent import Agent
 from phi.model.openai import OpenAIChat
 from models import  prompt
-import agents.小鎮賦能.query_projects
-import agents.小鎮賦能.analysis_projects
 
 # Load environment variables from .env file
 load_dotenv()
@@ -26,32 +24,23 @@ app.add_middleware(
 async def health():
     return {"result": "Healthy Server!"}
 
-# 建立三個專門的 agent
-edu_agent = Agent(
-   name="Education Agent",
-   role="查詢教育相關計畫",
-   tools=[agents.小鎮賦能.query_projects.get_edu_dataframe],
+def self_introduction():
+    return "我的名字叫做小明，我是一個 AI 聊天機器人，我可以幫助你進行自我介紹。"
+
+self_intro_agent = Agent(
+   name="Self-introduction Agent",
+   role="自我介紹",
+   tools=[self_introduction],
    show_tool_calls=True
 )
 
-ocean_agent = Agent(
-   name="Ocean Agent",
-   role="查詢海洋相關計畫",
-   tools=[agents.小鎮賦能.query_projects.get_ocean_dataframe],
-   show_tool_calls=True
-)
-
-tour_agent = Agent(
-   name="Tour Agent",
-   role="查詢遊程相關計畫",
-   tools=[agents.小鎮賦能.query_projects.get_tour_dataframe],
-   show_tool_calls=True
-)
+def analyse_project():
+    return "我是專案分析 Agent，我可以幫助你分析專案。"
 
 analysis_project_agent = Agent(
    name="Project analysis Agent",
-   role="以 SROI 分析計畫",
-   tools=[agents.小鎮賦能.analysis_projects.analyse_project],
+   role= "專案分析",
+   tools=[analyse_project],
    show_tool_calls=True
 )
 
@@ -62,18 +51,16 @@ agent_team = Agent(
         temperature = 1,
         timeout = 30
     ),
-   name="小鎮賦能團隊",
-   team=[edu_agent, ocean_agent, tour_agent, analysis_project_agent],
+   name="Agent Team",
+   team=[self_intro_agent, analysis_project_agent],
    add_history_to_messages=True,
    num_history_responses=3,
-   # instructions=["根據查詢內容選擇適當的agent執行任務"],
    show_tool_calls=False,
    tool_call_limit=1
 )
 
 @app.post("/prompt")
 async def prompt(prompt: prompt):
-    # json_params = json.dumps({"weight":["07", "12"]})
     response = agent_team.run(f"{prompt.message}", stream=False)
     # 尋找 assistant role 的最後一條訊息
     assistant_content = None
